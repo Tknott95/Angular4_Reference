6e6fc62208b47cf9146ba2e751170eeaf77dee99
Tknott95 <tknott95@hotmail.com>
2017-07-31 17:37:29 -0600

    Trev Tracker Done - TK

--- hikefolio/src/app/services/map/map.service.ts
+++ hikefolio/src/app/services/map/map.service.ts
@@ -34,7 +34,7 @@ export class MapService {
     L.tileLayer('https://api.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18,
-      id: 'mapbox.pencil',
+      id: 'mapbox.satellite',
       accessToken: apiToken
     }).addTo(map);
 
