09a0cdbf3aa103524af39607378d12aeeaf2bc81
Tknott95 <tknott95@hotmail.com>
2017-08-01 21:52:46 -0600

    Pulling in image service and working  || PHOTOBLOG - TK

--- Angular4_Projects/tk-photoGallery/src/app/components/gallery/gallery.component.ts
+++ Angular4_Projects/tk-photoGallery/src/app/components/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { ImageService } from './../../services/image.service';
 
 @Component({
