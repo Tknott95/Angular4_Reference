00d2965a51453e6d05327adb096565344e310304
Tknott95 <tknott95@hotmail.com>
2017-08-01 21:30:17 -0600

    Image Se3rvice MOCK DATA IN  || PHOTOBLOG - TK

---
+++ Angular4_Projects/tk-photoGallery/src/app/components/gallery/images/image-detail/image-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tk-image-detail',
+  templateUrl: './image-detail.component.html',
+  styleUrls: ['./image-detail.component.scss']
+})
+export class ImageDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Angular4_Projects/tk-photoGallery/src/app/services/image.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { MOCK_IMAGES } from './mock-data/images';
+
+@Injectable()
+export class ImageService {
+
+  constructor() { }
+
+}
