7ef4b936700553d8b7a8bd2a2d85ab48a91a037f
Tknott95 <tknott95@hotmail.com>
2017-08-03 01:32:22 -0600

    APP DONE ANIMS GREAT || stagAnims - TK

--- Ang4_Animations/StagAnims/src/app/app.component.ts
+++ Ang4_Animations/StagAnims/src/app/app.component.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import {
+  trigger, 
+  animate,
+  style, 
+  transition,
+  keyframes,
+  query,
+  stagger
+} from '@angular/animations';
 
-/* <!-- @AUTHOR tk@trevorknott.io --> */
+/*  @AUTHOR tk@trevorknott.io  */
 @Component({
   selector: 'tk-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+   trigger('arrayAnim', [
+      transition('* => *', [
+ 
+      query(':enter', style({opacity: 0}), {optional: true}),
+      
+      query(':enter', stagger('300ms', [
+        animate('1s ease-in', keyframes([
+          style({opacity: 0, transform: 'translateY(-75px)', offset: 0}),
+          style({opacity: .5, transform: 'translateY(35px)', offset: 0.3}),
+          style({opacity: 1, transform: 'translateY(0)', offset: 1})
+        ]))
+      ]), {optional: true}),
+
+      query(':leave', stagger('300ms', [
+        animate('1s ease-in', keyframes([
+          style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+          style({opacity: .5, transform: 'translateY(35px)', offset: 0.3}),
+          style({opacity: 0, transform: 'translateY(-75px)', offset: 1})
+        ]))
+      ]), {optional: true}),
+
+    ])
+   ])
+  ]
 })
+
 export class AppComponent {
   title = 'tk';
 
