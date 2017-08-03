83d9ce7618d0588c93014ba6e40507cc77e06e56
Tknott95 <tknott95@hotmail.com>
2017-08-03 01:08:09 -0600

    Basic Anim Setup - TK

--- Ang4_Animations/StagAnims/src/app/app.component.ts
+++ Ang4_Animations/StagAnims/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+/* <!-- @AUTHOR tk@trevorknott.io --> */
 @Component({
   selector: 'tk-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tk';
+
+  items = [];
+
+  constructor() {
+    this.items = ['this is animations', 'this is angular4', 'this is powerful for design', 'this works with golang well']
+  }
+
+  insertItem() {
+    this.items.push('this is better with comp')
+  }
+
+  removeItem() {
+    this.items.pop();
+  }
 }
--- Ang4_Animations/StagAnims/src/app/app.module.ts
+++ Ang4_Animations/StagAnims/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+/* <!-- @AUTHOR tk@trevorknott.io --> */
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
