4b90a36e42d775cc6488f77cbe80a0e20d8ae057
Tknott95 <tknott95@hotmail.com>
2017-08-02 20:16:33 -0600

    ImgFindR Basic MVP DONE styling needed || ImgFindR - TK

--- Angular4_Projects/ImgFindR/src/app/components/img-list/img-list.component.ts
+++ Angular4_Projects/ImgFindR/src/app/components/img-list/img-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ImageService } from '../../services/image/image.service';
 
 /* @AUTHOR tk@trevorknott.io */
+/* Using diff names in certain instances to help my brain really wrap my head around every concept*/
+/* In non practice projects code always keeps cleanest conventions */
 @Component({
   selector: 'tk-img-list',
   templateUrl: './img-list.component.html',
@@ -9,6 +11,7 @@ import { ImageService } from '../../services/image/image.service';
 })
 export class ImgListComponent implements OnInit {
   images: any[];
+  imagesReturned: boolean = false;
   imgSearchBtn: string ='Search for Images';
 
   constructor( private _imageService: ImageService) { }
@@ -16,10 +19,23 @@ export class ImgListComponent implements OnInit {
   ngOnInit() {
   }
 
-  searchImages(query_to_make: string) {
-    return this._imageService.getImage(query_to_make).subscribe(
-      data => console.log(data),
-      error => console.log(error),
+  /* Request from api comes back as an array called hits */
+  /* @TODO create a custom interface to handle results for elegent code :P*/
+  handleSuccess(res: any) {
+    this.imagesReturned = true;
+    this.images = res.hits;
+    console.log(res.hits)
+  }
+
+  handleError(res: any) {
+    this.imagesReturned = false;
+    console.log(res);
+  }
+
+  searchImages(query_to_search: string) {
+    return this._imageService.getImage(query_to_search).subscribe(
+      res => this.handleSuccess(res),
+      err => this.handleError(err),
       () => console.log("Request Complete!")
     )
   }
