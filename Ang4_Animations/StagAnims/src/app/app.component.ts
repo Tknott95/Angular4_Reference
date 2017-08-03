7190c31d5e869bab17ef2b68b97e6275958d3b37
Tknott95 <tknott95@hotmail.com>
2017-08-03 01:46:05 -0600

    APP DONE ANIMS GREAT || stagAnims - TK

--- Ang4_Animations/StagAnims/src/app/app.component.ts
+++ Ang4_Animations/StagAnims/src/app/app.component.ts
@@ -37,6 +37,16 @@ import {
       ]), {optional: true}),
 
     ])
+   ]),
+
+   trigger('quoteAnim', [
+     transition('* => *', [     
+       query('.col', style({ opacity: 0, transform: 'translateX(-40px)'})),
+       query('.col', stagger('500ms', [
+         animate('800ms 1.2s ease-out', style({opacity: 1, transform: 'translateX(0)'}))
+       ]))
+  
+     ])
    ])
   ]
 })
